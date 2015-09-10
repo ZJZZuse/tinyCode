176a379041b868990cf6a4a5556083de157e7005
447338871@qq.com <447338871@qq.com>
2015-09-10 19:04:21 +0800

    practice

--- gemTest/webdriver_test.rb
+++ gemTest/webdriver_test.rb
@@ -1,9 +1,17 @@
 require 'watir-webdriver'
+require 'nokogiri'
 
 
-b = Watir::Browser.new
+
+b = Watir::Browser.new :chrome
 b.goto("http://www.baidu.com")
 b.text_field(:id=>"kw").set("watir-webdriver")
 b.button(:id=>"su").click
-p b.html
+# p b.html
+
+doc = Nokogiri::HTML(b.html)
+
+doc.css('a').each do |link|
+  puts "#{link.content}, #{link['href']}"
+end
 
